961f296f6ced011fe99abf0a4d91485275c63442
wtmeersii <131147513+wtmeersii@users.noreply.github.com>
2023-06-14 08:15:36 -0700

    Update google_docs_tool.py

--- superagi/tools/plugins/google_docs_tool.py
+++ superagi/tools/plugins/google_docs_tool.py
@@ -1,28 +1,36 @@
 from superagi.tools import Tool
-from googleapiclient import discovery
-from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
 
-class GoogleDocsTool(Tool):
+class GoogleDocsPlugin(Tool):
     def __init__(self, credentials):
         super().__init__()
-        self.credentials = service_account.Credentials.from_service_account_info(credentials)
-        self.service = discovery.build('docs', 'v1', credentials=self.credentials)
+        creds = Credentials.from_service_account_info(credentials)
+        self.service = build('docs', 'v1', credentials=creds)
 
-        class GoogleDocsTool(Tool):
-    # ...
+    def read_document(self, document_id):
+        # Code to read a Google Doc
+        pass
 
-    def run(self, title):
-        document = self.service.documents().create().execute()
+    def write_document(self, document_id, content):
+        # Code to write to a Google Doc
+        pass
+
+    def read_document(self, document_id):
+        document = self.service.documents().get(documentId=document_id).execute()
+    return document
+
+    def write_document(self, document_id, content):
         requests = [
             {
-                'insertText': {
-                    'location': {
-                        'index': 1,
-                    },
-                    'text': title
-                }
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': content
             }
-        ]
-        result = self.service.documents().batchUpdate(
-            documentId=document['documentId'], body={'requests': requests}).execute()
-        return result
+        }
+    ]
+    result = self.service.documents().batchUpdate(
+        documentId=document_id, body={'requests': requests}).execute()
+    return result
